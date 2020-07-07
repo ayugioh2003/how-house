743cb2c8a673905ef0ca2c37b5e5dd17cf813204
chu <ayugioh2003@gmail.com>
2020-07-08 00:58:38 +0800

    Fix style lint error

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,16 @@
+// Fix Doc: https://stackoverflow.com/questions/47607602/how-to-add-a-tailwind-css-rule-to-css-checker
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {},
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['extends', 'tailwind'],
+      },
+    ],
+    'block-no-empty': null,
+    // 'unit-whitelist': ['em', 'rem', 's'],
+  },
 }
